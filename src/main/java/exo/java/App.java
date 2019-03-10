47c7501251cf444a95f61c40cb2d2f8d2dc337cc
Abraham94 <brams945@gmail.com>
2019-03-11 00:12:54 +0100

    test

---
+++ src/main/java/exo/java/App.java
@@ -0,0 +1,13 @@
+package exo.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
